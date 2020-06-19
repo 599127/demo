7bd77ad8bc94d51b5eb2763c6bc566f621948cfb
absamad <absamad@LIN19001276.corp.capgemini.com>
2020-06-19 11:26:46 +0530

    First Commit

---
+++ cgdemo/src/main/java/com/cg/cgdemo/CgdemoApplication.java
@@ -0,0 +1,13 @@
+package com.cg.cgdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgdemoApplication.class, args);
+	}
+
+}
---
+++ cgdemo/src/test/java/com/cg/cgdemo/CgdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.cgdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
