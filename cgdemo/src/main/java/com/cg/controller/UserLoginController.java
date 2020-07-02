bcf4accd73a339cbf18f03710f99627753034fdb
Amit009sh <amit.kumar.bhardwaj18@gmail.com>
2020-07-02 11:45:09 +0530

    resetProfile

--- cgdemo/src/main/java/com/cg/controller/UserLoginController.java
+++ cgdemo/src/main/java/com/cg/controller/UserLoginController.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.model.User;
 import com.cg.repository.UserRepository;
+import com.cg.security.util.SecurityUtility;
 import com.cg.services.UserService;
 
 @RestController
@@ -54,6 +56,25 @@ public class UserLoginController {
 		return new ResponseEntity<User>(localUser.get(), HttpStatus.NOT_FOUND);
 	}
 	
+	
+	
+	@PutMapping(value = "/user/resetProfile/{email}")
+	public ResponseEntity<User> updateUser(@PathVariable("userName") String id, @RequestBody User user) {
+		System.out.println("Updating User " + id);
+
+		Optional<User> currentUser = userRepository.findByEmail(user.getEmail());
+
+		if (!currentUser.isPresent()) {
+			System.out.println("User with id " + id + " not found");
+			return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+		}
+		User userInfo = currentUser.get();
+		userInfo.setFirstName(user.getFirstName());
+		userInfo.setLastName(user.getLastName());
+		userInfo.setPassword(SecurityUtility.passwordEncoder().encode(user.getPassword()));
+		userRepository.save(user);
+		return new ResponseEntity<User>(userInfo, HttpStatus.OK);
+	}
 	
 	@GetMapping("/user/all")
 	public ResponseEntity<List<User>> getAllUsers(){
